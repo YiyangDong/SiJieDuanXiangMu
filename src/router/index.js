f26eb6243cff2667292e8456c207cad28388cd47
YiyangDong <1151224780@qq.com>
2019-11-14 18:41:16 +0800

    更新的

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,12 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-import Deposit from "../components/Deposit.vue";
 
-import Cs from "../components/Cs.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    redirect: "/deposit"
-  },
-  {
-    path: "/deposit",
-
-    component: Deposit
-  },
-  {
-    path:'/cs',
-    component:Cs
-  }
+ 
 
 ];
 
